2b85b7f063df7b4790d5acd31d281e06a64809da
jorzen <jorzen2010@163.com>
2018-11-30 16:22:55 +0800

    ok

---
+++ dushuhui/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dushuhui.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Shudan()
+        {
+            return View();
+        }
+        public ActionResult Peiduren()
+        {
+            return View();
+        }
+        public ActionResult Dushubiji()
+        {
+            return View();
+        }
+        public ActionResult Chuangshirenshuo()
+        {
+            return View();
+        }
+        public ActionResult Xueyuanchengzhang()
+        {
+            return View();
+        }
+        public ActionResult Shu()
+        {
+            return View();
+        }
+        public ActionResult Ren()
+        {
+            return View();
+        }
+        public ActionResult Dushuying()
+        {
+            return View();
+        }
+
+    }
+}
